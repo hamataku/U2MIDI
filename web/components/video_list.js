df70c25da5071c21273f763dca41883f10a4220b
KajioNaoya <kjo.hrd.eik@gmail.com>
2023-03-28 00:32:57 +0900

    デザイン調整

--- web/components/video_list.js
+++ web/components/video_list.js
@@ -1,17 +1,17 @@
 Vue.component('video_list', {
   props: ['list', 'video_lists', 'title'],
   template:`
-      <div>
-        <h2>{{ title }}</h2>
-        <div class="row">
-          <div v-for="n in list" class="col-sm-6">
-            <div class="card" style="width: 18rem;">
-              <img :src="video_lists[n][2]" class="card-img-top">
-              <div class="card-body">
-                <h5 class="card-title"> {{ video_lists[n][0] }}</h5>
-                <a :href="'practice/' + n + '.html'" class="btn btn-primary btn-sm">練習モード</a>
-                <a :href="'scoring/' + n + '.html'" class="btn btn-primary btn-sm">採点モード</a>
-              </div>
+      <div class="m-4">
+        <div class="rounded-pill bg-primary px-3 py-2 mx-auto" style="width: fit-content">
+          <h4 class="text-center text-white m-0" style="width: fit-content">{{ title }}</h4>
+        </div> 
+        <div class="row justify-content-center">
+          <div v-for="n in list" class="card m-2" style="width: 18rem;">
+            <img :src="video_lists[n][2]" class="card-img-top">
+            <div class="card-body">
+              <h5 class="card-title"> {{ video_lists[n][0] }}</h5>
+              <a :href="'practice/' + n + '.html'" class="btn btn-primary btn-sm">練習モード</a>
+              <a :href="'scoring/' + n + '.html'" class="btn btn-success btn-sm">採点モード</a>
             </div>
           </div>
         </div>
